e53486d9dfa316d6b0d9bfb3a89e1a1090765a23
Piers O'Hanlon <piers.ohanlon@bbc.co.uk>
2022-07-28 09:50:51 +0100

    Fix for FastSwitching to include segment Duration * 1.5

--- src/streaming/StreamProcessor.js
+++ src/streaming/StreamProcessor.js
@@ -660,7 +660,7 @@ function StreamProcessor(config) {
     function _prepareForFastQualitySwitch(representationInfo) {
         // if we switch up in quality and need to replace existing parts in the buffer we need to adjust the buffer target
         const time = playbackController.getTime();
-        let safeBufferLevel = 1.5;
+        let safeBufferLevel = 1.5 * representationInfo.fragmentDuration;
         const request = fragmentModel.getRequests({
             state: FragmentModel.FRAGMENT_MODEL_EXECUTED,
             time: time + safeBufferLevel,
